447bc888974a7313a9d882396f54e11845f7bd25
MarkVann <markvann@gmail.com>
2017-08-01 17:40:02 +0800

    adjust gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -43,11 +43,11 @@ gulp.task('image', () => {
   const ignoreSrc = ['src/img/**/_*', 'src/img/_*/*'];
   const distPath = 'dist/img';
   const taskIgnore = gulp.src(ignoreSrc)
-    .pipe($.changed(distPath))
+    .pipe($.changed(distPath, { hasChanged: $.changed.compareContents }))
     .pipe($.size({ showFiles: true }))
     .pipe(gulp.dest(distPath));
   const taskImage = gulp.src(imageSrc)
-    .pipe($.changed(distPath))
+    .pipe($.changed(distPath, { hasChanged: $.changed.compareContents }))
     .pipe($.imagemin([
       imageminMozjpeg({ quality: 80 }),
       imageminPngquant({ quality: 75 }),
@@ -56,18 +56,15 @@ gulp.task('image', () => {
       // $.imagemin.optipng({ optimizationLevel: 5 }),
     ], { verbose: true }))
     .pipe(gulp.dest(distPath));
-  if (DEV_MODE) {
-    gulp.watch('src/img/**/*', () => sequence('image', 'reload'));
-  }
-  return merge(taskIgnore, taskImage);
+  return merge(taskIgnore, taskImage)
+    .on('end', () => {
+      server.reload();
+    });
 });
 
 gulp.task('pug', () => {
   const srcPath = ['src/html/**/*.pug', '!src/html/**/_*', '!src/html/include/*.pug'];
   const moment = require('moment');
-  if (DEV_MODE) {
-    gulp.watch('src/html/**/*.pug', () => sequence('pug', 'reload'));
-  }
   return gulp.src(srcPath)
     .pipe($.plumber())
     .pipe($.pug({
@@ -79,15 +76,15 @@ gulp.task('pug', () => {
       },
       verbose: true,
     }))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .on('end', () => {
+      server.reload();
+    });
 });
 
 gulp.task('stylus', () => {
-  const srcPath = ['src/css/**/*.styl', '!src/css/**/_*'];
+  const srcPath = ['src/css/*.styl', '!src/css/include/*'];
   const autoprefixer = require('autoprefixer');
-  if (DEV_MODE) {
-    gulp.watch('src/css/**/*.styl', () => sequence('stylus', 'stream'));
-  }
   return gulp.src(srcPath)
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
@@ -106,7 +103,8 @@ gulp.task('stylus', () => {
       }),
     ]))
     .pipe(DEV_MODE ? $.sourcemaps.write() : $.util.noop())
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('dist/css'))
+    .pipe(server.reload({ stream: true }));
 });
 
 gulp.task('js', () => {
@@ -118,12 +116,12 @@ gulp.task('js', () => {
     'src/js/index.js',
   ];
   const streams = entryList.map((entry) => {
-    const brow = browserify(entry, {
+    const bsfy = browserify(entry, {
       debug: DEV_MODE,
       cache: {},
       packageCache: {},
     });
-    brow.transform(babelify, {
+    bsfy.transform(babelify, {
       presets: ['es2015', 'stage-3'],
       plugins: [
         'transform-runtime',
@@ -133,10 +131,10 @@ gulp.task('js', () => {
         'transform-class-properties',
       ],
     });
-    brow.transform(envify({
+    bsfy.transform(envify({
       NODE_ENV: DEV_MODE ? 'development' : 'production',
     }));
-    const makeWatchify = (bundler, entry) => {
+    const createBundle = (bundler, entry) => {
       const path = require('path');
       const source = require('vinyl-source-stream');
       const buffer = require('vinyl-buffer');
@@ -154,19 +152,19 @@ gulp.task('js', () => {
           .pipe(DEV_MODE ? $.sourcemaps.write() : $.util.noop())
           .pipe(gulp.dest('dist/js'))
           .on('end', () => {
-            if (DEV_MODE) gulp.start('reload');
+            if (DEV_MODE) server.reload();
           });
       };
     };
-    let watchFunc;
-    if (!DEV_MODE) watchFunc = makeWatchify(brow, entry);
+    let bundleFunc;
+    if (!DEV_MODE) bundleFunc = createBundle(bsfy, entry);
     else {
-      const bundler = watchify(brow);
-      watchFunc = makeWatchify(bundler, entry);
-      bundler.on('update', watchFunc);
+      const bundler = watchify(bsfy);
+      bundleFunc = createBundle(bundler, entry);
+      bundler.on('update', bundleFunc);
       bundler.on('log', log);
     }
-    return watchFunc();
+    return bundleFunc();
   });
   return streams;
 });
@@ -189,7 +187,7 @@ gulp.task('lib', () => {
  */
 const server = require('browser-sync').create();
 
-gulp.task('server', () => {
+gulp.task('server', callback => {
   // const proxy = require('http-proxy-middleware')
   server.init({
     open: true,
@@ -202,17 +200,12 @@ gulp.task('server', () => {
       //   })
       // ]
     },
+  }, () => {
+    gulp.watch('src/img/**/*', ['image']);
+    gulp.watch('src/html/**/*.pug', ['pug']);
+    gulp.watch('src/css/**/*.styl', ['stylus']);
+    callback();
   });
-});
-
-gulp.task('reload', (callback) => {
-  server.reload();
-  callback();
-});
-
-gulp.task('stream', (callback) => {
-  server.stream({ match: '**/*.css' });
-  callback();
 });
 
 gulp.task('rimraf', (callback) => {
